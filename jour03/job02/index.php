bb08800841c0c2f356abc4e36bfc60d3a8e106a6
alex <alex.lovati@laplateforme.io>
2022-03-17 15:27:41 +0100

    fini jour03 oubli

---
+++ jour03/job02/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>JavaScript</title>
+    
+    <script type="text/javascript" src='script.js'></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+</head>
+<body>
+    <header>
+    </header>
+    <main style="display:flex; flex-direction:column;">
+        <div id="melangee"></div>
+        <div id="melangees" style="display:flex;border:1px solid black;"></div>
+        <div id="rangees" style="display:flex;border:1px solid black;"></div>
+        <button id="button">Mélanger</button>
+        <script src="script.js"></script>
+    </main>
+    <footer>
+    </footer>
+</body>
+</html>
---
+++ jour03/job02/script.js
@@ -0,0 +1,45 @@
+for (i = 1; i <= 6; i++) {
+    $("#rangees").append("<img src='arc" + i + ".png'/>");
+}
+
+$("#rangees").children().click(function (src) {
+    src.remove;
+    $("#rangees").append(src.target);
+
+    if ($("#rangees").children().length == 6) {
+        var win = true;
+        for (i = 0; i < 6; i++) {
+
+            if ($("#rangees").children()[i].src !== 'http://localhost/runtrack3/jour03/job02/arc' + (i + 1) + '.png') {
+                var endLose = document.createElement("p");
+                endLose.innerText = "Vous avez perdu"
+                endLose.style.color = "red";
+                document.body.appendChild(endLose);
+                win = false;
+                $("#button").click(function () {
+                    location.reload();
+                })
+                break;
+            }
+        }
+
+        if (win) {
+            var endWin = document.createElement("p");
+            endWin.innerText = "Vous avez gagné"
+            endWin.style.color = "green";
+            document.body.appendChild(endWin);
+            $("#button").click(function () {
+                location.reload();
+            })
+
+        }
+    }
+});
+
+$("#button").click(function () {
+    for (i = 0; i <= 6; i++) {
+        var index = Math.floor(Math.random() * $("#rangees").children().length - 1) + 1;
+        $("#melangees").append($("#rangees").children()[index]);
+    }
+
+})
