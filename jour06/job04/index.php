407f2b53a270d44fdc76c74cbaecc23e2935450d
alex <alex.lovati@laplateforme.io>
2022-03-22 16:15:31 +0100

    fini job04.6

---
+++ jour06/job04/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Media</title>
+    <link rel="stylesheet" type="text/css" media="screen" href="style.css">
+</head>
+<body>
+    <header>
+        <div class="hamburger-menu">
+            <input id="menu__toggle" type="checkbox" />
+            <label class="menu__btn" for="menu__toggle">
+                <span></span>
+            </label>
+
+            <ul class="menu__box">
+                <a class="menu__item" href="#">Accueil</a>
+                <a class="menu__item" href="#">Présentation</a>
+                <a class="menu__item" href="#">Contact</a>
+            </ul>
+        </div>
+    </header>
+    <main>
+    </main>
+</body>
+</html>
+
+
+
+
